a83ebe00f7b47a7aa53fd782d675bc5d8f4ee461
ymzkjpx <ymzkjpx@gmail.com>
2022-01-23 20:18:27 +0900

    OS環境変数からクレデンシャル情報を取得

--- src/main/java/company/aws/s3/MakeS3Bucket.java
+++ src/main/java/company/aws/s3/MakeS3Bucket.java
@@ -1,31 +1,21 @@
 package company.aws.s3;
 
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import software.amazon.awssdk.services.s3.S3Client;
 
 
 public class MakeS3Bucket {
 
+    private String bucketName = "ymzkjpx-test-buket" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyMMddHHmm"));
+
     private static S3Client s3Client;
     public void create() {
-        String bucketName = "ymzkjpx-test-buket";
         System.out.println(bucketName);
-
-        Region region = Region.AP_NORTHEAST_1;
-        String accessKey = "xxx";
-        String secret = "xxx";
-        AwsCredentials credentials = AwsBasicCredentials.create(accessKey, secret);
-
-
-        s3Client = S3Client.builder()
-                .region(region)
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
-                .build();
-
-        CreateBucket.createBucket(s3Client, bucketName, region);
+        MyS3Client myS3Client = new MyS3Client();
+        System.out.println(myS3Client);
+        CreateBucket.createBucket(myS3Client.s3Client(), bucketName, myS3Client.region());
     }
 }
---
+++ src/main/java/company/aws/s3/MyS3Client.java
@@ -0,0 +1,30 @@
+package company.aws.s3;
+
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+public class MyS3Client {
+    private S3Client s3Client;
+    private Region region = Region.AP_NORTHEAST_1;
+
+    public MyS3Client() {
+        this.s3Client = concreate();
+    }
+
+    private S3Client concreate() {
+        S3Client s3Client = S3Client.builder()
+                .region(region)
+                .credentialsProvider(ProfileCredentialsProvider.create("design-pattern"))
+                .build();
+        return s3Client;
+    }
+
+    public S3Client s3Client() {
+        return s3Client;
+    }
+
+    public Region region() {
+        return region;
+    }
+}
